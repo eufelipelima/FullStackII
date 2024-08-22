a159c87d673d30e74554f56b21adf544d9de1d75
eufelipelima <157855349+eufelipelima@users.noreply.github.com>
2024-08-21 23:20:17 -0300

    Add files via upload

---
+++ Controle/animalCtrl.js
@@ -0,0 +1,169 @@
+import Animal from "../Modelo/animal.js";
+import Adotante from "../Modelo/adotante.js";
+
+export default class AnimalCtrl {
+
+    gravar(requisicao, resposta) {
+        resposta.type('application/json');
+        if (requisicao.method === 'POST' && requisicao.is('application/json')) {
+            const dados = requisicao.body;
+            const nome = dados.nome;
+            const raca = dados.raca;
+            const sexo = dados.sexo;
+            const especie = dados.especie;
+            const dataNascimento = dados.dataNascimento;
+            const dataCadastro = dados.dataCadastro;
+            const adotante_codigo = dados.adotante.codigo;
+
+            if (nome && raca && sexo && especie
+                && dataNascimento && dataCadastro && adotante_codigo > 0) {
+                const adotante = new Adotante(adotante_codigo);
+                const animal = new Animal(0, nome, raca, sexo, especie,
+                    dataNascimento, dataCadastro, adotante
+                );
+                //resolver a promise
+                animal.gravar().then(() => {
+                    resposta.status(200).json({
+                        "status": true,
+                        "codigoGerado": animal.codigo,
+                        "mensagem": "Animal incluído com sucesso!"
+                    });
+                })
+                    .catch((erro) => {
+                        resposta.status(500).json({
+                            "status": false,
+                            "mensagem": "Erro ao registrar o animal:" + erro.message
+                        });
+                    });
+            }
+            else {
+                resposta.status(400).json({
+                    "status": false,
+                    "mensagem": "Por favor, os dados do animal segundo a documentação da API!"
+                });
+            }
+        }
+        else {
+            resposta.status(400).json({
+                "status": false,
+                "mensagem": "Por favor, utilize o método POST para cadastrar um animal!"
+            });
+        }
+    }
+
+    atualizar(requisicao, resposta) {
+        resposta.type('application/json');
+        if ((requisicao.method === 'PUT' || requisicao.method === 'PATCH') && requisicao.is('application/json')) {
+            const dados = requisicao.body;
+            const codigo = dados.codigo;
+            const nome = dados.nome;
+            const raca = dados.raca;
+            const sexo = dados.sexo;
+            const especie = dados.especie;
+            const dataNascimento = dados.dataNascimento;
+            const dataCadastro = dados.dataCadastro;
+            const adotante_codigo = dados.adotante.codigo;
+            if (codigo && nome && raca && sexo && especie && dataNascimento && dataCadastro && adotante_codigo > 0) {
+                const adotante = new Adotante(adotante_codigo);
+                const animal = new Animal(codigo, nome, raca, sexo, especie,
+                    dataNascimento, dataCadastro, codigo);
+                //resolver a promise
+                animal.atualizar().then(() => {
+                    resposta.status(200).json({
+                        "status": true,
+                        "mensagem": "Animal atualizado com sucesso!"
+                    });
+                })
+                    .catch((erro) => {
+                        resposta.status(500).json({
+                            "status": false,
+                            "mensagem": "Erro ao atualizar o animal:" + erro.message
+                        });
+                    });
+            }
+            else {
+                resposta.status(400).json({
+                    "status": false,
+                    "mensagem": "Por favor, informe todos os dados do animal segundo a documentação da API!"
+                });
+            }
+        }
+        else {
+            resposta.status(400).json({
+                "status": false,
+                "mensagem": "Por favor, utilize os métodos PUT ou PATCH para atualizar um animal!"
+            });
+        }
+    }
+
+    excluir(requisicao, resposta) {
+        resposta.type('application/json');
+        if (requisicao.method === 'DELETE' && requisicao.is('application/json')) {
+            const dados = requisicao.body;
+            const codigo = dados.codigo;
+            if (codigo) {
+                const animal = new Animal(codigo);
+                //resolver a promise
+               animal.atualizar().then(() => {
+                    resposta.status(200).json({
+                        "status": true,
+                        "mensagem": "Animal excluído com sucesso!"
+                    });
+                })
+                    .catch((erro) => {
+                        resposta.status(500).json({
+                            "status": false,
+                            "mensagem": "Erro ao excluir o animal:" + erro.message
+                        });
+                    });
+            }
+            else {
+                resposta.status(400).json({
+                    "status": false,
+                    "mensagem": "Por favor, informe o código do animal!"
+                });
+            }
+        }
+        else {
+            resposta.status(400).json({
+                "status": false,
+                "mensagem": "Por favor, utilize o método DELETE para excluir um animal!"
+            });
+        }
+    }
+
+
+    consultar(requisicao, resposta) {
+        resposta.type('application/json');
+        //express, por meio do controle de rotas, será
+        //preparado para esperar um termo de busca
+        let termo = requisicao.params.termo;
+        if (!termo) {
+            termo = "";
+        }
+        if (requisicao.method === "GET") {
+            const animal = new Animal();
+            animal.consultar(termo).then((listaAnimais) => {
+                resposta.json(
+                    {
+                        status: true,
+                        listaAnimais
+                    });
+            })
+                .catch((erro) => {
+                    resposta.json(
+                        {
+                            status: false,
+                            mensagem: "Não foi possível obter os animais: " + erro.message
+                        }
+                    );
+                });
+        }
+        else {
+            resposta.status(400).json({
+                "status": false,
+                "mensagem": "Por favor, utilize o método GET para consultar animais!"
+            });
+        }
+    }
+}
